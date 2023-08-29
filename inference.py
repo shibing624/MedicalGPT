e498e0d89670bb4f9b49072e4c328a64db1556f8
shibing624 <shibing624@126.com>
2023-08-29 10:43:46 +0800

    update save to jsonl.

--- inference.py
+++ inference.py
@@ -91,7 +91,7 @@ def main():
     parser.add_argument('--data_file', default=None, type=str,
                         help="A file that contains instructions (one instruction per line)")
     parser.add_argument('--interactive', action='store_true', help="run in the instruction mode (single-turn)")
-    parser.add_argument('--predictions_file', default='./predictions.json', type=str)
+    parser.add_argument('--predictions_file', default='./predictions.jsonl', type=str)
     parser.add_argument('--resize_emb', action='store_true', help='Whether to resize model token embeddings')
     parser.add_argument('--gpus', default="0", type=str)
     parser.add_argument('--only_cpu', action='store_true', help='only use CPU for inference')
@@ -218,7 +218,10 @@ def main():
             results.append({"Input": prompt, "Output": response})
 
         with open(args.predictions_file, 'w', encoding='utf-8') as f:
-            json.dump(results, f, ensure_ascii=False, indent=2)
+            for entry in results:
+                json.dump(entry, f, ensure_ascii=False)
+                f.write('\n')
+        print(f'save to {args.predictions_file}, size: {len(results)}')
 
 
 if __name__ == '__main__':
