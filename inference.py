c35321f12e4bcc7bafc120f8c3da1887703fb1fd
shibing624 <shibing624@126.com>
2023-09-10 22:53:08 +0800

    update inf.

--- inference.py
+++ inference.py
@@ -46,7 +46,7 @@ def stream_generate_answer(
         context_len=2048,
         stop_str="</s>",
 ):
-    streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=False)
+    streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=True)
     input_ids = tokenizer(prompt).input_ids
     max_src_len = context_len - max_new_tokens - 8
     input_ids = input_ids[-max_src_len:]
