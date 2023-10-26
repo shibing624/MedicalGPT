75083bd19b9b3e326a6671ef346db63193f90f87
shibing624 <shibing624@126.com>
2023-10-26 14:13:06 +0800

    update gradio version.

--- gradio_demo.py
+++ gradio_demo.py
@@ -3,7 +3,7 @@
 @author:XuMing(xuming624@qq.com)
 @description:
 
-pip install gradio
+pip install gradio>=3.50.2
 pip install mdtex2html
 """
 import argparse
@@ -11,7 +11,6 @@ import os
 from threading import Thread
 
 import gradio as gr
-import mdtex2html
 import torch
 from peft import PeftModel
 from transformers import (
@@ -37,39 +36,6 @@ MODEL_CLASSES = {
 }
 
 
-@torch.inference_mode()
-def stream_generate_answer(
-        model,
-        tokenizer,
-        prompt,
-        device,
-        max_new_tokens=512,
-        do_sample=True,
-        temperature=0.7,
-        top_p=0.8,
-        repetition_penalty=1.0,
-        context_len=2048,
-):
-    """Generate answer from prompt with GPT and stream the output"""
-    streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=True)
-    input_ids = tokenizer(prompt).input_ids
-    max_src_len = context_len - max_new_tokens - 8
-    input_ids = input_ids[-max_src_len:]
-    generation_kwargs = dict(
-        input_ids=torch.as_tensor([input_ids]).to(device),
-        max_new_tokens=max_new_tokens,
-        do_sample=do_sample,
-        temperature=temperature,
-        top_p=top_p,
-        repetition_penalty=repetition_penalty,
-        streamer=streamer,
-    )
-    thread = Thread(target=model.generate, kwargs=generation_kwargs)
-    thread.start()
-
-    yield from streamer
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_type', default=None, type=str, required=True)
@@ -77,11 +43,11 @@ def main():
     parser.add_argument('--lora_model', default="", type=str, help="If None, perform inference on the base model")
     parser.add_argument('--tokenizer_path', default=None, type=str)
     parser.add_argument('--template_name', default="vicuna", type=str,
-                        help="Prompt template name, eg: alpaca, vicuna, baichuan, chatglm2 etc.")
+                        help="Prompt template name, eg: alpaca, vicuna, baichuan2, chatglm2 etc.")
     parser.add_argument('--gpus', default="0", type=str)
     parser.add_argument('--only_cpu', action='store_true', help='only use CPU for inference')
     parser.add_argument('--resize_emb', action='store_true', help='Whether to resize model token embeddings')
-    parser.add_argument('--share', default=True, help='Share gradio')
+    parser.add_argument('--share', action='store_true', help='Share gradio')
     parser.add_argument('--port', default=8081, type=int, help='Port of gradio demo')
     args = parser.parse_args()
     print(args)
@@ -89,50 +55,6 @@ def main():
     if args.only_cpu is True:
         args.gpus = ""
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
-
-    def postprocess(self, y):
-        if y is None:
-            return []
-        for i, (message, response) in enumerate(y):
-            y[i] = (
-                None if message is None else mdtex2html.convert((message)),
-                None if response is None else mdtex2html.convert(response),
-            )
-        return y
-
-    gr.Chatbot.postprocess = postprocess
-
-    def parse_text(text):
-        """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
-        lines = text.split("\n")
-        lines = [line for line in lines if line != ""]
-        count = 0
-        for i, line in enumerate(lines):
-            if "```" in line:
-                count += 1
-                items = line.split('`')
-                if count % 2 == 1:
-                    lines[i] = f'<pre><code class="language-{items[-1]}">'
-                else:
-                    lines[i] = f'<br></code></pre>'
-            else:
-                if i > 0:
-                    if count % 2 == 1:
-                        line = line.replace("`", "\`")
-                        line = line.replace("<", "&lt;")
-                        line = line.replace(">", "&gt;")
-                        line = line.replace(" ", "&nbsp;")
-                        line = line.replace("*", "&ast;")
-                        line = line.replace("_", "&lowbar;")
-                        line = line.replace("-", "&#45;")
-                        line = line.replace(".", "&#46;")
-                        line = line.replace("!", "&#33;")
-                        line = line.replace("(", "&#40;")
-                        line = line.replace(")", "&#41;")
-                        line = line.replace("$", "&#36;")
-                    lines[i] = "<br>" + line
-        text = "".join(lines)
-        return text
 
     load_type = torch.float16
     if torch.cuda.is_available():
@@ -174,73 +96,42 @@ def main():
     prompt_template = get_conv_template(args.template_name)
     stop_str = tokenizer.eos_token if tokenizer.eos_token else prompt_template.stop_str
 
-    def reset_user_input():
-        return gr.update(value='')
-
-    def reset_state():
-        return [], []
-
-    def predict(
-            now_input,
-            chatbot,
-            history,
-            max_new_tokens,
-            temperature,
-            top_p
-    ):
-        chatbot.append((parse_text(now_input), ""))
-        history = history or []
-        history.append([now_input, ''])
-
+    def predict(message, history):
+        """Generate answer from prompt with GPT and stream the output"""
+        history.append([message, ''])
         prompt = prompt_template.get_prompt(messages=history)
-        response = ""
-        for new_text in stream_generate_answer(
-                model,
-                tokenizer,
-                prompt,
-                device,
-                max_new_tokens=max_new_tokens,
-                temperature=temperature,
-                top_p=top_p,
-        ):
-            stop = False
-            pos = new_text.find(stop_str)
-            if pos != -1:
-                new_text = new_text[:pos]
-                stop = True
-            response += new_text
-            history[-1][-1] = response
-            chatbot[-1] = (parse_text(now_input), parse_text(response))
-            yield chatbot, history
-            if stop:
-                break
+        streamer = TextIteratorStreamer(tokenizer, timeout=60.0, skip_prompt=True, skip_special_tokens=True)
+        input_ids = tokenizer(prompt).input_ids
+        context_len = 2048
+        max_new_tokens = 1024
+        max_src_len = context_len - max_new_tokens - 8
+        input_ids = input_ids[-max_src_len:]
+        generation_kwargs = dict(
+            input_ids=torch.as_tensor([input_ids]).to(device),
+            streamer=streamer,
+            max_new_tokens=max_new_tokens,
+            do_sample=True,
+            temperature=0.7,
+            top_p=0.8,
+            repetition_penalty=1.0,
+        )
+        thread = Thread(target=model.generate, kwargs=generation_kwargs)
+        thread.start()
 
-    with gr.Blocks() as demo:
-        gr.HTML("""<h1 align="center">MedicalGPT</h1>""")
-        gr.Markdown(
-            "> 为了促进医疗行业大模型的开放研究，本项目开源了[MedicalGPT](https://github.com/shibing624/MedicalGPT)医疗大模型")
-        chatbot = gr.Chatbot()
-        with gr.Row():
-            with gr.Column(scale=4):
-                with gr.Column(scale=12):
-                    user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(
-                        container=False)
-                with gr.Column(min_width=32, scale=1):
-                    submitBtn = gr.Button("Submit", variant="primary")
-            with gr.Column(scale=1):
-                emptyBtn = gr.Button("Clear History")
-                max_length = gr.Slider(
-                    0, 4096, value=512, step=1.0, label="Maximum length", interactive=True)
-                top_p = gr.Slider(0, 1, value=0.8, step=0.01,
-                                  label="Top P", interactive=True)
-                temperature = gr.Slider(
-                    0, 1, value=0.7, step=0.01, label="Temperature", interactive=True)
-        history = gr.State([])
-        submitBtn.click(predict, [user_input, chatbot, history, max_length, temperature, top_p], [chatbot, history],
-                        show_progress=True)
-        submitBtn.click(reset_user_input, [], [user_input])
-        emptyBtn.click(reset_state, outputs=[chatbot, history], show_progress=True)
-    demo.queue().launch(share=args.share, inbrowser=True, server_name='0.0.0.0', server_port=args.port)
+        partial_message = ""
+        for new_token in streamer:
+            if new_token != stop_str:
+                partial_message += new_token
+                yield partial_message
+
+    gr.ChatInterface(
+        predict,
+        chatbot=gr.Chatbot(),
+        textbox=gr.Textbox(placeholder="Ask me question", lines=4, scale=9),
+        title="MedicalGPT",
+        description="为了促进医疗行业大模型的开放研究，本项目开源了[MedicalGPT](https://github.com/shibing624/MedicalGPT)医疗大模型",
+        theme="soft",
+    ).queue().launch(share=args.share, inbrowser=True, server_name='0.0.0.0', server_port=args.port)
 
 
 if __name__ == '__main__':
