117f5101a61af825f44ea19be58597ea1fdf6868
shibing624 <shibing624@126.com>
2024-04-28 04:29:46 +0800

    update llama3 system template.

--- template.py
+++ template.py
@@ -306,14 +306,16 @@ reference: https://github.com/facebookresearch/llama/blob/cfc3fc8c1968d390eb830e
 register_conv_template(
     Conversation(
         name="llama2",
-        system_prompt="<<SYS>>\nYou are a helpful, respectful and honest assistant. "
-                      "Always answer as helpfully as possible, while being safe. "
-                      "Your answers should not include any harmful, unethical, racist, sexist, "
-                      "toxic, dangerous, or illegal content. "
-                      "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
-                      "If a question does not make any sense, or is not factually coherent, "
-                      "explain why instead of answering something not correct. "
-                      "If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n",
+        system_prompt=(
+            "<<SYS>>\nYou are a helpful, respectful and honest assistant. "
+            "Always answer as helpfully as possible, while being safe. "
+            "Your answers should not include any harmful, unethical, racist, sexist, "
+            "toxic, dangerous, or illegal content. "
+            "Please ensure that your responses are socially unbiased and positive in nature.\n\n"
+            "If a question does not make any sense, or is not factually coherent, "
+            "explain why instead of answering something not correct. "
+            "If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n"
+        ),
         messages=[],
         roles=("[INST]", "[/INST]"),
         prompt="[INST] {query} [/INST]",
